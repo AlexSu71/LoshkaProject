4bf8b3fc82a0015dfda0eac263cce4ed2d586409
Alexander <76393715+AlexSu71@users.noreply.github.com>
2024-04-14 17:10:17 +0200

    Update IUnitOfWork.cs

--- LoshkaDAL/Interfaces/IUnitOfWork.cs
+++ LoshkaDAL/Interfaces/IUnitOfWork.cs
@@ -3,7 +3,7 @@
     public interface IUnitOfWork
     {
         IUnitRepository UnitRepository { get; }
-        // Добавить свойства для других репозиториев здесь
+    
         int Complete();
         Task<int> CompleteAsync();
     }
